0a576c41ce7245d1b17fc4cfacfb53591a018d65
Alejandro villagran <alejandro18maestro@gmail.com>
2018-08-05 21:44:15 -0400

    se implemento sesiones

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,25 @@ Route::put('mensajes/{id}', 'MessagesController@update')->name('messages.update'
 
 Route::delete('mensajes/{id}', 'MessagesController@destroy')->name('messages.destroy');
 
+//---------------------------login----------------------------------------------------------------------
 
+Route::get('login', 'Auth\LoginController@showLoginForm');
+Route::post('login', 'Auth\LoginController@login');
+//-------------------------------------login-------------------------------------------------------
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+        // Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+        // Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//control+p  router.php  y luego control+f auth que es una funcion con las rutas
